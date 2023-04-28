d55083574f9b5387e309b749860f327df97c0eb1
jpablopachar <jpablopachar1993@gmail.com>
2023-04-28 14:27:13 -0500

    Creación de vistas para gestionar Cines

--- Controllers/CinemasController.cs
+++ Controllers/CinemasController.cs
@@ -1,5 +1,6 @@
 using app_movies.Data.Services;
 using app_movies.Data.Static;
+using app_movies.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,6 +22,66 @@ namespace app_movies.Controllers
             var cinemas = await _cinemasService.GetAllAsync();
 
             return View(cinemas);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Logo, Name, Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+
+            await _cinemasService.AddAsync(cinema);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinema = await _cinemasService.GetByIdAsync(id);
+
+            if (cinema == null) return View("NotFound");
+
+            return View(cinema);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var cinema = await _cinemasService.GetByIdAsync(id);
+
+            if (cinema == null) return View("NotFound");
+
+            return View(cinema);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Logo, Name, Description")] Cinema cinema)
+        {
+            if (!ModelState.IsValid) return View(cinema);
+
+            await _cinemasService.UpdateAsync(id, cinema);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinema = await _cinemasService.GetByIdAsync(id);
+
+            if (cinema == null) return View("NotFound");
+
+            return View(cinema);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cinema = await _cinemasService.GetByIdAsync(id);
+
+            if (cinema == null) return View("NotFound");
+
+            await _cinemasService.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
